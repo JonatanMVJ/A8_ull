a70d6c6af8b9876cd5a6145198d3d2a5933567ca
JONATAN_MAZUCA_VALENZUELA <a.21308051280382@cbtis128.edu.mx>
2024-04-26 18:24:28 +0000

    as

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,363 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late PageController _pageController;
+  int _currentPageIndex = 0;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _currentPageIndex);
+    _startTimer();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 2), (timer) {
+      _currentPageIndex = (_currentPageIndex + 1) % 7;
+      _pageController.animateToPage(
+        _currentPageIndex,
+        duration: Duration(milliseconds: 500),
+        curve: Curves.ease,
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      title: "Sounds_Music",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Sounds_Music",
+            style: TextStyle(color: Color(0xffd79e00)),
+          ),
+          backgroundColor: Colors.black,
+          centerTitle: true,
+        ),
+        body: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() {
+              _currentPageIndex = index;
+            });
+          },
+          children: [
+            // Contenido del primer tab (imagen)
+            Container(
+              padding: EdgeInsets.all(10),
+              child: InkWell(
+                onTap: () {
+                  print("aasdasd");
+                },
+                child: Card(
+                  elevation: 40,
+                  color: Colors.grey[300],
+                  shadowColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        ),
+                        child: Image(
+                          image: AssetImage("assets/images/piano.jpg"),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "YAMAHA",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // Contenido del segundo tab
+            Container(
+              padding: EdgeInsets.all(10),
+              child: InkWell(
+                onTap: () {
+                  print("aasdasd");
+                },
+                child: Card(
+                  elevation: 40,
+                  color: Colors.grey[300],
+                  shadowColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        ),
+                        child: Image(
+                          image: AssetImage("assets/images/bat.jpg"),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Ibanez",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // Contenido del tercer tab
+            Container(
+              padding: EdgeInsets.all(10),
+              child: InkWell(
+                onTap: () {
+                  print("aasdasd");
+                },
+                child: Card(
+                  elevation: 40,
+                  color: Colors.grey[300],
+                  shadowColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        ),
+                        child: Image(
+                          image: AssetImage("assets/images/poder.png"),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "JBL",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // Contenido del cuarto tab
+            Container(
+              padding: EdgeInsets.all(10),
+              child: InkWell(
+                onTap: () {
+                  print("aasdasd");
+                },
+                child: Card(
+                  elevation: 40,
+                  color: Colors.grey[300],
+                  shadowColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        ),
+                        child: Image(
+                          image: AssetImage("assets/images/guitar.jpg"),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Fender",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // Contenido del quinto tab
+            Container(
+              padding: EdgeInsets.all(10),
+              child: InkWell(
+                onTap: () {
+                  print("aasdasd");
+                },
+                child: Card(
+                  elevation: 40,
+                  color: Colors.grey[300],
+                  shadowColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        ),
+                        child: Image(
+                          image: AssetImage("assets/images/bajo.jpg"),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Taylor",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // Contenido del sexto tab
+            Container(
+              padding: EdgeInsets.all(10),
+              child: InkWell(
+                onTap: () {
+                  print("aasdasd");
+                },
+                child: Card(
+                  elevation: 40,
+                  color: Colors.grey[300],
+                  shadowColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        ),
+                        child: Image(
+                          image: AssetImage("assets/images/pas jbl.jpg"),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Pasiva JBL",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // Contenido del séptimo tab
+            Container(
+              padding: EdgeInsets.all(10),
+              child: InkWell(
+                onTap: () {
+                  print("aasdasd");
+                },
+                child: Card(
+                  elevation: 40,
+                  color: Colors.grey[300],
+                  shadowColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        ),
+                        child: Image(
+                          image: AssetImage("assets/images/sax.jpg"),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Saxofón",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
